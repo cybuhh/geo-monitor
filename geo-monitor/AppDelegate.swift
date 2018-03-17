be0cb0e8cc71ac78a3ee9c6185376f69280c7bdc
cybuhh <mateusz.cyburt@gmail.com>
2018-03-17 06:03:14 +0100

    Initial Commit

---
+++ geo-monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  geo-monitor
+//
+//  Created by cybuhh on 17/03/2018.
+//  Copyright © 2018 cybuhh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ geo-monitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  geo-monitor
+//
+//  Created by cybuhh on 17/03/2018.
+//  Copyright © 2018 cybuhh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
